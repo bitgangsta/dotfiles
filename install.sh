9df27b75b92f2370699f2be2d4e216175d26eb3c
Daniel Jabbour <daniel@amoe.ba>
2014-01-16 00:48:22 -0800

    Reverting katie's stuff and adding mine

--- install.sh
+++ install.sh
@@ -20,8 +20,8 @@ vim -u ~/.vimrc.bundles +BundleInstall +qa
 
 ### Customization
 
-echo "Enter the name of the computer: "
-read computer_name
+# echo "Enter the name of the computer: "
+# read computer_name
 
 remove_and_link(){
   local target="$2"
@@ -34,27 +34,5 @@ remove_and_link(){
   ln -snf "${source}" "${target}"
 }
 
-# Messages
-remove_and_link "$HOME/Dropbox/SoftwareSettings/Messages.$computer_name" "$HOME/Library/Messages"
-
-# Skype
-remove_and_link "$HOME/Dropbox/SoftwareSettings/kategengler.$computer_name.skype" "$HOME/Library/Application Support/Skype/kategengler"
-
-# KeyRemap4Mac
-remove_and_link "$HOME/Dropbox/SoftwareSettings/KeyRemap4MacBook" "$HOME/Library/Application Support/KeyRemap4MacBook"
-
-# iTerm2
-remove_and_link "$HOME/Dropbox/SoftwareSettings/iterm.plist" "$HOME/Library/Preferences/com.googlecode.iterm2.plist"
-
-# Adium
-remove_and_link "$HOME/Dropbox/Adium 2.0" "$HOME/Library/Application Support/Adium 2.0"
-
-# RubyMine
-remove_and_link "$HOME/Dropbox/RubyMine Config/RubyMine60" "$HOME/Library/Preferences/RubyMine60"
-
 # SSH Config
 remove_and_link "$HOME/dotfiles/ssh_config" "$HOME/.ssh/config"
-
-if [[ ! -d "$HOME/dev" ]]; then
-  mkdir "$HOME/dev"
-fi
