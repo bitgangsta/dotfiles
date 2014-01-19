fc1864a92b9a3055f825e18a816fe1eb56c60785
Daniel Jabbour <daniel@amoe.ba>
2014-01-18 17:45:52 -0800

    Setting up tmux and some other refactoring

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-rm -rf $HOME/.vim/bundle/vundle
-
 for name in *; do
   target="$HOME/.$name"
   if [ -e "$target" ]; then
@@ -15,16 +13,23 @@ for name in *; do
   fi
 done
 
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-vim -u ~/.vimrc.bundles +BundleInstall +qa
-
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-
-
 ### Customization
 
 # echo "Enter the name of the computer: "
 # read computer_name
+
+git_clone_or_update() {
+  local giturl="$1"
+  local target="$2"
+	
+  if [[ ! -d "${target}" ]] ; then
+	git clone "${giturl}" "${target}"
+  else
+	echo "Path already contains git repo, updating: ${target}"
+    cd "${target}"
+	git pull
+  fi
+}
 
 remove_and_link(){
   local target="$2"
@@ -36,6 +41,26 @@ remove_and_link(){
 
   ln -snf "${source}" "${target}"
 }
+
+brew_install_ifn() {  
+  for pkg in $*; do
+	if brew list -1 | grep -q "^${pkg}\$"; then
+	  echo "Package '$pkg' is already installed (skipping)."
+	else
+	  brew install "${pkg}"
+	fi
+  done
+}
+
+git_clone_or_update https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+vim -u ~/.vimrc.bundles +BundleInstall +qa
+
+git_clone_or_update https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+git_clone_or_update https://github.com/erikw/tmux-powerline.git ~/.tmux-powerline
+
+# Install some shiz
+brew_install_ifn tmux reattach-to-user-namespace
 
 # SSH Config
 remove_and_link "$HOME/dotfiles/ssh_config" "$HOME/.ssh/config"
